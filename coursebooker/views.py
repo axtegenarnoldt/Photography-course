a322c1d81825f5c2c941712a8dfe9f04ada3b101
Josefine Axtegen Arnoldt <axtegenarnoldt@gmail.com>
2024-12-16 13:47:45 +0000

    add course detail html and content to that file
    make chnage to views.py and add course content to detail.html

--- coursebooker/views.py
+++ coursebooker/views.py
@@ -1,15 +1,22 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
-from django.views import generic, View
+from django.views.generic import ListView, DetailView, View
 from .models import Course
 
 # Create your views here.
-class CourseList(generic.ListView):
+class CourseList(ListView):
 
     model = Course
     queryset = Course.objects.filter(status=1).order_by('difficulty',
                                                       '-created_on')
     template_name = "index.html"
     paginate_by = 6
+
+class CourseDetail(DetailView):
+    model = Course
+    template_name = 'course_detail.html'
+    slug_field = 'slug'
+    slug_url_conflicts = 'truncate_slash'
 
 def course_detail(request, course_slug):
     course = get_object_or_404(Course, slug=course_slug)
