92431ccbfc05d65b265f5f46d041d08a59f6653b
Josefine Axtegen Arnoldt <axtegenarnoldt@gmail.com>
2024-12-21 18:46:06 +0000

    add cloudinary settings

--- photocourse/settings.py
+++ photocourse/settings.py
@@ -159,3 +159,10 @@ cloudinary.config(
     api_secret=os.environ.get("CLOUDINARY_API_SECRET")
 )
 
+CLOUDINARY = {
+    'cloud_name': os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    'api_key': os.environ.get("CLOUDINARY_API_KEY"),
+    'api_secret': os.environ.get("CLOUDINARY_API_SECRET")
+}
+
+
